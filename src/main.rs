dc66ec8fa6c415b6bf5950fc982ce552786ceca9
lightningund <lightningundies0826@gmail.com>
2026-05-21 15:03:12 -0500

    made a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> eframe::Result {
+	println!("Hello, world!");
+	let options = eframe::NativeOptions {
+		viewport: egui::ViewportBuilder::default().with_inner_size([500.0, 500.0]),
+		..Default::default()
+	};
+	eframe::run_native(
+		"My egui App",
+		options,
+		Box::new(|_| {
+			Ok(Box::<MyApp>::default())
+		}),
+	)
+}
+
+struct MyApp {
+	name: String,
+	age: u32,
+}
+
+impl Default for MyApp {
+	fn default() -> Self {
+		Self {
+			name: "Arthur".to_owned(),
+			age: 42,
+		}
+	}
+}
+
+impl eframe::App for MyApp {
+	fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+		egui::CentralPanel::default().show_inside(ui, |ui| {
+			ui.heading("My egui Application");
+			ui.horizontal(|ui| {
+				let name_label = ui.label("Your name: ");
+				ui.text_edit_singleline(&mut self.name)
+					.labelled_by(name_label.id);
+			});
+			ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+			if ui.button("Increment").clicked() {
+				self.age += 1;
+			}
+			ui.label(format!("Hello '{}', age {}", self.name, self.age));
+
+			// ui.image(egui::include_image!(
+			// 	"../../../crates/egui/assets/ferris.png"
+			// ));
+		});
+	}
 }
---
+++ todo.md
@@ -0,0 +1,25 @@
+image display
+zooming
+panning
+image loading
+image saving
+pixel by pixel edits
+color picker
+color palette
+layers
+layer enabling
+layer reordering
+layer renaming
+layer blend modes
+layer opacity
+selecting
+expanding selection
+subtracting selection
+deleting selected
+moving selected
+copying selected
+pasting
+rotating
+canvas resize
+canvas expand/shrink
+undo/redo
