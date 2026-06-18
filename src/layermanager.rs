f4ddfb9c5d21924302bc3e0b212124b5466d7d91
lightningund <lightningundies0826@gmail.com>
2026-06-18 14:58:47 -0500

    added layer opacity and a proper layer settings window

--- src/layermanager.rs
+++ src/layermanager.rs
@@ -18,10 +18,16 @@ impl std::fmt::Display for DimensionError {
 
 impl std::error::Error for DimensionError {}
 
+#[derive(Default, Clone, Eq, PartialEq, Debug)]
+pub struct LayerSettings {
+	pub opacity: u8,
+	pub name: String,
+}
+
 pub struct Layer {
 	pub image: TextureImage,
 	pub enabled: bool,
-	pub name: String,
+	pub settings: LayerSettings,
 }
 
 #[derive(Default)]
@@ -29,7 +35,8 @@ pub struct LayerManager {
 	size: PixelCoord,
 	layers: Vec<Layer>,
 	curr_layer: usize,
-	renaming_layer: Option<usize>,
+	backup_settings: Option<LayerSettings>,
+	configuring_layer: Option<usize>, // If we are currently editing the settings of a layer in the popout, the idx is stored here
 }
 
 impl LayerManager {
@@ -55,7 +62,14 @@ impl LayerManager {
 			return Err(DimensionError{ target_size: self.size, actual_size: img.data.size });
 		}
 
-		self.layers.push(Layer{ image: img, enabled: true, name: format!("Layer {}", self.layers.len()) });
+		self.layers.push(Layer{
+			image: img,
+			enabled: true,
+			settings: LayerSettings {
+				opacity: 255,
+				name: format!("Layer {}", self.layers.len())
+			}
+		});
 		Ok(self.layers.last().unwrap())
 	}
 
@@ -66,6 +80,51 @@ impl LayerManager {
 		self.add_layer(
 			TextureImage::new(ColorImage::filled(self.size, Color32::TRANSPARENT), ctx)
 		).expect("Layer size somehow incorrect?");
+	}
+
+	fn draw_layer_settings(&mut self, ui: &mut Ui) {
+		if let Some(idx) = self.configuring_layer {
+			let cfg = &mut self.layers[idx].settings;
+			let mut open = true;
+			let window = egui::Window::new("Layer Settings")
+				.order(egui::Order::Foreground)
+				.collapsible(false)
+				.default_pos(ui.clip_rect().center())
+				.open(&mut open);
+
+			// TODO: add settings for blending mode, delete
+			window.show(ui.ctx(), |ui| {
+				ui.horizontal(|ui| {
+					let name_label = ui.label("Name:");
+					ui.text_edit_singleline(&mut cfg.name).labelled_by(name_label.id);
+				});
+				ui.horizontal(|ui| {
+					let opacity_label = ui.label("Opacity:");
+					ui.add(egui::Slider::new(&mut cfg.opacity, 0..=255)).labelled_by(opacity_label.id);
+				});
+
+				ui.horizontal(|ui| {
+					ui.with_layout(egui::Layout::right_to_left(egui::Align::TOP), |ui| {
+						if ui.button("Cancel").clicked() {
+							self.configuring_layer = None;
+							if let Some(backup) = &self.backup_settings {
+								*cfg = backup.clone();
+							}
+						}
+						if ui.button("Save").clicked() {
+							self.configuring_layer = None;
+						}
+					});
+				});
+			});
+
+			if !open {
+				self.configuring_layer = None;
+				if let Some(backup) = &self.backup_settings {
+					*cfg = backup.clone();
+				}
+			}
+		}
 	}
 
 	pub fn draw_panel(&mut self, ui: &mut Ui) {
@@ -79,21 +138,17 @@ impl LayerManager {
 				ui.horizontal(|ui| {
 					ui.checkbox(&mut layer.enabled, "");
 
-					if Some(idx) == self.renaming_layer {
-						if ui.text_edit_singleline(&mut layer.name).lost_focus() {
-							self.renaming_layer = None;
+					ui.selectable_value(&mut self.curr_layer, idx, format!("{}", layer.settings.name)).context_menu(|ui| {
+						if ui.button("Settings").clicked() {
+							self.configuring_layer = Some(idx);
+							self.backup_settings = Some(layer.settings.clone());
 						}
-					} else {
-						ui.selectable_value(&mut self.curr_layer, idx, format!("{}", layer.name)).context_menu(|ui| {
-							// TODO: add settings for blending mode, opacity, delete
-							if ui.button("Rename").clicked() {
-								self.renaming_layer = Some(idx);
-							}
-						});
-					}
+					});
 				});
 			}
 		});
+
+		self.draw_layer_settings(ui);
 	}
 
 	pub fn draw_layers(&self, ui: &mut Ui) {
@@ -102,7 +157,7 @@ impl LayerManager {
 			if !layer.enabled { continue; }
 
 			img_pos.max = layer.image.size.max;
-			ui.put(img_pos, egui::Image::new(&layer.image.handle));
+			ui.put(img_pos, egui::Image::new(&layer.image.handle).tint(Color32::from_white_alpha(layer.settings.opacity)));
 		}
 	}
 
@@ -114,7 +169,9 @@ impl LayerManager {
 				let mut pixel = Color32::TRANSPARENT;
 				let idx = (x as usize) + (y as usize) * self.size[0];
 				for l in &self.layers {
-					pixel = pixel.blend(l.image.data.pixels[idx]);
+					let mut l_pixel = l.image.data.pixels[idx];
+					l_pixel = l_pixel.gamma_multiply_u8(l.settings.opacity);
+					pixel = pixel.blend(l_pixel);
 				}
 				image::Rgba(pixel.to_array())
 			}
